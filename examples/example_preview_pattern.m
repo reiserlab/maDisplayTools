702c75c5bb0e14623358040fd192f6697166d3d9
Lisa Ferguson <taylorl@janelia.hhmi.org>
2025-11-10 10:57:53 -0500

    first commit

---
+++ examples/example_preview_pattern.m
@@ -0,0 +1,4 @@
+% The command to preview a pattern is as follows. Replace path with the
+% your path to the provided pattern, or a different .pat file
+
+maDisplayTools.preview_pat('/Users/lisaferguson/Documents/PC/Programming/Reiser/maDisplayTools/examples/pat0001_4RowSqGrate_Matlab.pat');
